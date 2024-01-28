486cb73070d2c41339accc8556bf1b5bc7eb2612
Hitesh Puri <h.puri@accenture.com>
2024-01-28 18:22:52 +0530

    Maintained state on reloading the page

--- src/utility/auth.jsx
+++ src/utility/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext(null);
 
@@ -7,14 +7,36 @@ export const AuthProvider = ({ children }) => {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
 
+  useEffect(() => {
+    // Check localStorage for user data on page reload
+    const storedUser = localStorage.getItem("user");
+    const storedEmail = localStorage.getItem("email");
+    const storedPassword = localStorage.getItem("password");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+      setEmail(JSON.parse(storedEmail));
+      setPassword(JSON.parse(storedPassword));
+    }
+  }, []);
+
   const login = (user, email, password) => {
     setUser(user);
     setEmail(email);
     setPassword(password);
+
+    // Store user data in localStorage
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("email", JSON.stringify(email));
+    localStorage.setItem("password", JSON.stringify(password));
   };
 
   const logout = () => {
     setUser(null);
+
+    // Remove user data from localStorage on logout
+    localStorage.removeItem("user");
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
   };
 
   return (
